6da4eb8cec9ec0661d2c70d54e6f636e3e1eb7e5
Rupert Garcia <rupert.garcia.ph@gmail.com>
2023-07-01 13:37:03 +0800

    Separated the Program into Classes and did a more Object Oriented Programming approach

--- ConversionCalculator.cs
+++ ConversionCalculator.cs
@@ -1,51 +1,73 @@
 using System;
-using MyNameSpace;
 
 namespace MyNameSpace
 {
-    public class NumberSystembolab
+    public class NumberSystemProgram
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("--------------------------------");        
+            NumberSystemMenu menu = new NumberSystemMenu();
+            menu.Start();
+        }
+    }
+
+    public class NumberSystemMenu
+    {
+        private bool running;
+
+        public void Start()
+        {
+            running = true;
+
+            Console.WriteLine("--------------------------------");
             Console.WriteLine("Learning About Number Systems");
             Console.WriteLine("--------------------------------");
 
-            while (true)
+            while (running)
             {
-                Console.WriteLine("\nSelect an option:");
-                Console.WriteLine("1. Lessons");
-                Console.WriteLine("2. Number System Converter");
-                Console.WriteLine("3. Have fun!");
-                Console.WriteLine("4. Exit");
-                Console.WriteLine("----------------------");
-                Console.Write("Enter your choice (1-4): ");
+                DisplayMenu();
                 string input = Console.ReadLine();
+                ProcessInput(input);
+            }
+        }
 
-                switch (input)
-                {
-                    case "1":
+        private void DisplayMenu()
+        {
+            Console.WriteLine("\nSelect an option:");
+            Console.WriteLine("1. Lessons");
+            Console.WriteLine("2. Number System Converter");
+            Console.WriteLine("3. Have fun!");
+            Console.WriteLine("4. Exit");
+            Console.WriteLine("----------------------");
+            Console.Write("Enter your choice (1-4): ");
+        }
 
-                        break;
+        private void ProcessInput(string input)
+        {
+            switch (input)
+            {
+                case "1":
+                    
+                    break;
 
-                    case "2":
-                        ExecuteConversion conversion = new ExecuteConversion();
-                        conversion.ConvertNumberSystem();
-                        break;
+                case "2":
+                    ExecuteConversion conversion = new ExecuteConversion();
+                    conversion.ConvertNumberSystem();
+                    break;
 
-                    case "3":
-                        ExecuteRecreational recreation = new ExecuteRecreational();
-                        recreation.RecreationalGames();
-                        break;
+                case "3":
+                    ExecuteRecreational recreation = new ExecuteRecreational();
+                    recreation.RecreationalGames();
+                    break;
 
-                    case "4":
-                        Console.WriteLine("Exiting the program...");
-                        return;
+                case "4":
+                    Console.WriteLine("Exiting the program...");
+                    running = false;
+                    break;
 
-                    default:
-                        Console.WriteLine("Invalid choice. Please try again.");
-                        break;
-                }
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
             }
         }
     }
